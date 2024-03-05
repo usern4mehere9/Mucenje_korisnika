b00bdc854ad4a743b8a05f02c7a1ea36602cf50b
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2024-03-05 11:57:16 +0100

    Add files via upload

---
+++ Mucenje_korisnika/IGRA.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mucenje_korisnika
+{
+    abstract public class Igra
+    {
+        abstract public void Pocni(int n);
+        abstract public string Odgovor(int Pokusaj);
+    }
+
+    internal class Laksi : Igra
+    {
+        private int Zamisljeni, maksimalan;
+
+        public override string Odgovor(int Pokusaj)
+        {
+            if (Pokusaj > maksimalan)
+            {
+                return "Van opsega";
+            }
+
+            if (Pokusaj > Zamisljeni)
+            {
+                return "Manji";
+            }
+            else if (Pokusaj < Zamisljeni)
+            {
+                return "Veci";
+            }
+            else if (Pokusaj == Zamisljeni)
+            {
+                return "Pogodjen";
+            }
+
+            return string.Empty;
+        }
+
+        public override void Pocni(int n)
+        {
+            Random razmisljam = new Random();
+            Zamisljeni = (1 + razmisljam.Next()) % n;
+            maksimalan = n;
+        }
+    }
+
+    internal class Tezi : Igra
+    {
+        private int opseg, max, min;
+
+        public static bool Kraj(int max, int min)
+        {
+            if (Math.Abs(max - min) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string Odgovor(int Pokusaj)
+        {
+            int delta_min, delta_max;
+            delta_min = Math.Abs(min - Pokusaj);
+            delta_max = Math.Abs(max - Pokusaj);
+
+            if (Pokusaj > opseg)
+            {
+                return "Van opsega";
+            }
+
+
+            if (delta_min == delta_max)
+            {
+                min = Pokusaj;
+
+                if (!Kraj(max, min))
+                {
+                    return "Veci";
+                }
+                return "Pogodjen";
+            }
+            else if (delta_min > delta_max)
+            {
+                max = Pokusaj;
+                if (!Kraj(max, min))
+                {
+                    return "Manji";
+                }
+                return "Pogodjen";
+            }
+            else if (delta_min < delta_max)
+            {
+                min = Pokusaj;
+                if (!Kraj(max, min))
+                {
+                    return "Veci";
+                }
+                return "Pogodjen";
+            }
+
+            return string.Empty;
+        }
+
+        public override void Pocni(int n)
+        {
+            opseg = n;
+            max = n;
+            min = 1;
+        }
+    }
+
+}
---
+++ Mucenje_korisnika/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Mucenje_korisnika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tezina;
+            int max, uneti;
+            string odgovor;
+
+            Igra igra;
+
+            Console.Write("Unesite max opsega: ");
+            max = int.Parse(Console.ReadLine());
+
+            Console.Write("Izabrite tezinu igre[true - laka | false - teska]: ");
+            tezina = bool.Parse(Console.ReadLine());
+
+            if (tezina)
+            {
+                igra = new Laksi();
+                igra.Pocni(max);
+            }
+            else
+            {
+                igra = new Tezi();
+                igra.Pocni(max);
+            }
+
+            do
+            {
+                Console.Write("Unesite broj: ");
+                uneti = int.Parse(Console.ReadLine());
+                odgovor = igra.Odgovor(uneti);
+                Console.WriteLine("Zamisljeni broj je: {0}", odgovor);
+            } while (odgovor != "Pogodjen");
+        }
+    }
+}
